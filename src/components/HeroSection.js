c2c6e531d14f3bfff592fa8a4b7e5191446b3a6d
Robbie08 <08ortiz.roberto@gmail.com>
2021-01-01 23:34:51 -0800

    Push base of project - only have hero set up and working

---
+++ src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from './Button';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection (){
+    return (
+        <div className='hero-container'>
+            <video src='/videos/hero-video2.mp4' autoPlay loop muted />
+            <h1>Hi, need a Software Engineer?</h1>
+            <p>I am a motived student that is always looking for a challenge</p>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonsSize='btn--large' >
+                    My Projects
+                </Button>
+                <Button className='btns' buttonStyle='btn--primary' buttonsSize='btn--large' >
+                    My Projects
+                </Button>
+            </div>
+        </div>
+
+    )
+}
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import {Button} from './Button';
+
+
+function Navbar() {
+    // we need to set a state to keep track of nav bar
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    
+    // create a function to handle the hamburger button click
+    const handleClick = () => setClick(!click);
+
+    // create a function to handle the navigation bar
+    const closeNavBar = () => setClick(false);
+
+    // create function that displays button 
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }
+        else {
+            setButton(true);
+        }
+    };
+
+    // include this use of hook to prevent the button from 
+    // appearing when application renders in mobile mode 
+    useEffect(() => {
+        showButton();
+    }, [] );
+
+    window.addEventListener('resize', showButton);
+    return(
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className='navbar-logo' onClick={closeNavBar}>
+                    Robert O.
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+
+                    {/* This will be our nav bar items */}
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeNavBar}>Home</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/resume' className='nav-links' onClick={closeNavBar}>Resume</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/github' className='nav-links' onClick={closeNavBar}>Github</Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/projects' className='nav-links-mobile' onClick={closeNavBar}>Projects</Link>
+                        </li>
+                    </ul>
+                    { button && <Button buttonStyle='btn--outline'>Projects</Button> }
+                </div>
+            </nav>
+        </>
+    );
+}
+export default Navbar;
