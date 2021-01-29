3e9c2673f2cdc2d4b7401affb0cdcbeecf463dba
Robbie08 <08ortiz.roberto@gmail.com>
2021-01-28 22:22:56 -0800

    Fixed about me section typo

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,11 @@ function About(){
                 <p className='about-bio-paragraph'>
                 I'm a Junior at <b>San Diego State University</b> working towards earning a <b>B.S. 
                 in Computer Science</b>. My passion for Computer Science started with a simple 
-                Hello World program in C++. Since then, I have worked on countless <b>Mobile 
-                and Web applications</b>, allowing me to learn new languages, tools, and frameworks 
+                Hello World program in C++. Since then, I have worked on countless <b>Mobile</b> 
+                and <b>Web applications</b>, allowing me to learn new languages, tools, and frameworks 
                 like <b>Java, JavaScript, React.js, Python, Git</b>, and a couple of other ones.
                 I have recently started delving into the world of 
-                Artificial Intelligence and Natural Language Processing. 
+                Artificial Intelligence and Computational Linguistics. 
                 </p>
                 <p className='about-bio-paragraph'>
                 I currently work as a <b>Teaching Assistant (TA)</b> for <b>The Computer Science Department</b> at <b>San Diego State Univeristy</b>.
